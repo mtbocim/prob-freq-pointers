fccc5903685c4c6fef93fc251000011348817d39
clchen-arcadia <clchen.arcadia@gmail.com>
2022-09-30 19:18:45 -0400

    Solve average-pair and format can-construct-word

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,43 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
-}
+/** Accepts a sorted array of numbers and a number targetAvg.
+ * Function returns a boolean if a pair of numbers exist whose average is
+ * the given targetAvg
+ */
 
+// averagePair([1, 2, 3], 2.5);         // true
+// averagePair([3, 3, 6, 12, 19], 8);   // false
+// averagePair([1, 2, 3], 2);           // true
+// averagePair([], 0);                  // false
+
+//observation: since the arr is sorted, if first entry> avg, fails fast
+// and also if last entry < avg, fails fast
+
+//we should use two pointers
+
+function averagePair(numbers, targetAvg) {
+  if (numbers.length === 0) {
+    return false;
+  }
+
+  let leftIndex = 0;
+  let rightIndex = numbers.length - 1;
+
+  debugger
+
+  while (numbers[leftIndex] <= targetAvg && numbers[rightIndex] >= targetAvg
+    && leftIndex !== rightIndex) {
+    let tempAverage = (numbers[leftIndex] + numbers[rightIndex]) / 2;
+    if (tempAverage === targetAvg) {
+      return true;
+    }
+    else if (tempAverage > targetAvg) {
+      rightIndex--;
+    }
+    else if (tempAverage < targetAvg) {
+      leftIndex++;
+    }
+  }
+
+  return false;
+}
--- can-construct-word.js
+++ can-construct-word.js
@@ -8,7 +8,7 @@
 //   // true -- can build "abc" with letters "abcd"
 
 // canConstructWord('aabb', 'bcabad');
-  // true -- can build "aabbcc" with those letters
+// true -- can build "aabbcc" with those letters
 
 //count the number of the letters in the "word"
 //we don't care if letters has extras
@@ -28,11 +28,11 @@ function canConstructWord(word, letters) {
     const letterCount = letterFreqCount(letters);
 
     //compare 1st counter to 2nd
-    for(key in wordLetterCount){
-        if(letterCount[key]===undefined){
+    for (key in wordLetterCount) {
+        if (letterCount[key] === undefined) {
             return false;
         }
-        if(letterCount[key]<wordLetterCount[key]){
+        if (letterCount[key] < wordLetterCount[key]) {
             return false;
         }
     }
@@ -43,17 +43,17 @@ function canConstructWord(word, letters) {
 /**Accepts a string (case insensitive), returns an object with single letters as keys,
  * and their counts.
  */
-function letterFreqCount(str){
+function letterFreqCount(str) {
     //return object with count of letters
     let obj = {};
-    for(let char of str){
+    for (let char of str) {
         char = char.toLowerCase();
-        if(obj[char]===undefined){
+        if (obj[char] === undefined) {
             obj[char] = 1;
-        }else{
+        } else {
             obj[char]++;
         }
     }
 
-    return obj //better names might be: letterCount, freqCount....
+    return obj; //better names might be: letterCount, freqCount....
 }
